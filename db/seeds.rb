192ebfbaf4b8075f02b7707a975060af95285d1d
Rojina Thapa <rojinat@ekbana.com>
2017-07-06 22:52:13 +0545

    Initial comit

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Contact.create(first_name: 'Archana', last_name: 'Thapa', email: 'archana@gmail.com')
+Contact.create(first_name: 'Rojina', last_name: 'Thapa', email: 'rojina@gmail.com')
+Contact.create(first_name: 'Anooj', last_name: 'Thapa', email: 'anoojthapa@gmail.com')
